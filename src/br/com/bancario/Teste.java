29f60533d55f16ef6a9948ebc3f8985e3f53637d
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 14:46:36 -0300

    Atribuindo valores

--- src/br/com/bancario/Teste.java
+++ src/br/com/bancario/Teste.java
@@ -4,6 +4,9 @@ public class Teste {
     public static void main(String[] args) {
         
         Conta cc = new Conta();
+        cc.saldo = 50.0;
+        cc.agencia = 123;
+        cc.numero = 321;
 
     }
 }
