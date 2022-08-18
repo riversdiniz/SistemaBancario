09ab4835b9b8980d00df2d07bab60370a1b462fa
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 14:41:25 -0300

    Estrutura da classe conta pronta.

--- src/br/com/bancario/Conta.java
+++ src/br/com/bancario/Conta.java
@@ -18,4 +18,16 @@ public class Conta {
         
     }
 
+    public void depositar(double valor){
+        this.saldo += valor;
+    }
+
+    public void retirar(double valor){
+        this.saldo -= valor;
+    }
+
+    public double verifcarSaldo(){
+        return this.saldo;
+    }
+
 }
