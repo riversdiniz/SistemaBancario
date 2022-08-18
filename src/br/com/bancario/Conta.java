78009c5e860508ef5fbeea9bf572aafa82f8c6b9
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 16:23:01 -0300

    Removendo os comentarios da documentação.

--- src/br/com/bancario/Conta.java
+++ src/br/com/bancario/Conta.java
@@ -41,42 +41,13 @@ public class Conta {
         this.numero = numero;
     }
 
-    /**
-     * Realiza um depósito na conta corrente
-     * 
-     * @param agencia o número da agencia
-     * @param numero  o número da conta
-     * @param Valor   o Valor que será depositado
-     * @return O número do protcolo de confirmação
-     */
-
-    /**
-     * Acrescena valor do Saldo da Conta
-     * 
-     * @param valor que será depositado
-     * @see depositar
-     */
-
     public void depositar(double valor) {
         this.saldo += valor;
     }
 
-    /**
-     * Retira um valor do Saldo da Conta
-     * 
-     * @param valor que seá retirado
-     * @see depositar
-     */
-
     public void retirar(double valor) {
         this.saldo -= valor;
     }
-
-    /**
-     * Verifica o Saldo da Conta
-     * 
-     * @return Valor do Saldo da Conta
-     */
 
     public double verifcarSaldo() {
         return this.saldo;
