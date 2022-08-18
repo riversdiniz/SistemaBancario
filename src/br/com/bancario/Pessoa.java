878f0d0c43da3d05f790e60ee9e272dce89186f6
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 16:26:48 -0300

    Removendo o comentario.

--- src/br/com/bancario/Pessoa.java
+++ src/br/com/bancario/Pessoa.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 
 public class Pessoa implements Serializable {
 
-    // private static final long serialVersionUID = 1L;
     private String nome;
 
     public String getNome() {
