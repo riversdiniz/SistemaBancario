d8f8401e9a0dbc7400c5ab968374f1b7ab709cfc
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 16:18:24 -0300

    Criando a classe pessoa.

---
+++ src/br/com/bancario/Pessoa.java
@@ -0,0 +1,18 @@
+package br.com.bancario;
+
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
+    // private static final long serialVersionUID = 1L;
+    private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
