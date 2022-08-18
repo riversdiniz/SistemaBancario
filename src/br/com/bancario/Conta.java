2ce5dd44e4a1a03a8617b20ca9cee5abaf4ee179
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 14:37:32 -0300

    Populando a estrutura.

--- src/br/com/bancario/Conta.java
+++ src/br/com/bancario/Conta.java
@@ -5,5 +5,17 @@ public class Conta {
     int agencia;
     int numero;
     double saldo;
-    
+
+    public Conta(){
+
+    }
+
+    public Conta(int agencia, int numero, double saldo){
+
+        this.agencia =  agencia;
+        this.numero = numero;
+        this.saldo =  saldo;
+        
+    }
+
 }
