76f62bd85dae7d8a68bbcaf2428629e1e154a7b6
River Diniz <river-s-diniz@hotmail.com>
2022-08-18 16:05:49 -0300

    Documentando o projeto.

--- src/br/com/bancario/Conta.java
+++ src/br/com/bancario/Conta.java
@@ -1,34 +1,69 @@
 package br.com.bancario;
 
+/**
+ * Classe que abstrai uma Conta Bancária
+ * 
+ * @author River Diniz
+ * @version 1.0
+ */
+
 public class Conta {
-    
+
     // Atributos de Classes
     int agencia;
     int numero;
     double saldo;
 
-    public Conta(){
+    public Conta() {
 
     }
 
     // Construtor de Classe
-    public Conta(int agencia, int numero, double saldo){
-        this.agencia =  agencia;
+    public Conta(int agencia, int numero, double saldo) {
+        this.agencia = agencia;
         this.numero = numero;
-        this.saldo =  saldo;
-        
+        this.saldo = saldo;
+
     }
 
-    // Depositar da Classe Conta
-    public void depositar(double valor){
+    /**
+     * Realiza um depósito na conta corrente
+     * 
+     * @param agencia o número da agencia
+     * @param numero  o número da conta
+     * @param Valor   o Valor que será depositado
+     * @return O número do protcolo de confirmação
+     */
+
+    /**
+     * Acrescena valor do Saldo da Conta
+     * 
+     * @param valor que será depositado
+     * @see depositar
+     */
+
+    public void depositar(double valor) {
         this.saldo += valor;
     }
 
-    public void retirar(double valor){
+    /**
+     * Retira um valor do Saldo da Conta
+     * 
+     * @param valor que seá retirado
+     * @see depositar
+     */
+
+    public void retirar(double valor) {
         this.saldo -= valor;
     }
 
-    public double verifcarSaldo(){
+    /**
+     * Verifica o Saldo da Conta
+     * 
+     * @return Valor do Saldo da Conta
+     */
+
+    public double verifcarSaldo() {
         return this.saldo;
     }
 
